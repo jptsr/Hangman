1dce939326968775fcc1084f4507e755a5b15d47
Julie <juptsr.becode@gmail.com>
2022-03-09 09:49:22 +0100

    initialization folder + files (html/css/js/font/img)

---
+++ assets/js/sketch.js
@@ -0,0 +1,14 @@
+/**
+ * propose a hidden word
+ * allow letters' selection
+ * verify if selected letter belongs to the word
+ * count the number of failure (lives)
+ * check if game over
+ * check if word has been found
+ * -------------------------------------------------------------------- GO FURTHER
+ * keep in memory all guessed letter
+ * only let the player guess a letter once
+ * add an image for each step
+ * check keayboard recognition
+ */
+
