2ad34193172ad97afda394e38c66b580acf76b28
Julie <juptsr.becode@gmail.com>
2022-03-09 17:11:06 +0100

    display keyboard + matching letters + counter

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,10 +1,4 @@
 /**
- * create an array of words in wordArray.js
- * take a random word in array
- * allow letters' selection
- * verify if selected letter belongs to the word
- * count the number of failure (lives)
- * check if game over
  * check if word has been found
  * -------------------------------------------------------------------- GO FURTHER
  * keep in memory all guessed letter
@@ -14,24 +8,22 @@
  */
 
 
-
 const ulWord = document.getElementById('ul_word');
+const ulKeyboard = document.getElementById('ul_keyboard');
+const keyboard = document.getElementById('letters');
+const p = document.getElementById('counter');
 const btn = document.getElementById('new_word');
+
+// console.log(p);
+
+let count = 0;
+p.textContent = `${count} / 10`;
+
 
 
 // CHOOSE A RANDOM WORD
 let actual_word = words[Math.floor(Math.random() * words.length)];
 console.log(`actual word: ${actual_word}`);
-
-
-// CLICK => CHANGE WORD
-btn.addEventListener('click', (actual_word) => {
-    // actual_word = words[Math.floor(Math.random() * words.length)];
-    // console.log(`actual word: ${actual_word}`);
-    // return actual_word;
-
-    document. location. reload();
-});
 
 
 // SPLIT RANDOM WORD IN ARRAY OF LETTERS
@@ -53,7 +45,7 @@ let mapLetter = mappingLetter(actual_word);
 console.log(mapLetter); // notation: console.log(`map letter: ${mapLetter}`) renvoie [object object]
 
 
-// DISPLAY ALL LETTER AS A LIST
+// DISPLAY WORD'S LETTERS AS A LIST
 let displayrandomWord = (mapL) => {
     // console.log('displayrandomWord function is ok');
 
@@ -64,12 +56,12 @@ let displayrandomWord = (mapL) => {
             // console.log(element.visible);
 
             let li = document.createElement('li');
-            li.setAttribute('class', 'Letterslist');
+            li.setAttribute('class', 'lettersList');
             ulWord.appendChild(li);
             li.textContent = element.letter;
         }else{
             let li = document.createElement('li');
-            li.setAttribute('class', 'Letterslist');
+            li.setAttribute('class', 'lettersList');
             ulWord.appendChild(li);
             li.textContent = '_';
         }
@@ -80,9 +72,127 @@ let wordIsDisplayed = displayrandomWord(mapLetter);
 
 
 // CREATE KEYBOARD
+let keyboard_letters = [];
+let createKeyboard = () => {
+    // console.log('createKeyboard function is ok');
+    
+    let i = 0;
+    for(keyCode = 65; keyCode <= 90; keyCode++){
+        keyboard_letters[i] = String.fromCharCode(keyCode);
+        // console.log(keyboard_letters[i]);
+        i++;       
+    }
+
+    console.log(`keyboard letters: ${keyboard_letters}`);
+    return keyboard_letters;
+}
+
+let keyboard_created = createKeyboard();
+
+
+// MAP KEYBOARD
+let mapKeyboard = (keyboard) => {
+    // console.log('mapKeyboard function is ok');
+
+    let final_keyboard = keyboard.map((key) => {
+        return {
+            keyLetter: key,
+            selected: false
+        }
+    });
+    console.log(final_keyboard);
+    return final_keyboard;
+};
+
+let keyboardMapping = mapKeyboard(keyboard_created);
+
+
+// DISPLAY KEYBOARD
+let displayKeyboard = (keyboard1) => {
+    // console.log('displayKeyboard function is ok');
+
+    keyboard1.forEach(element => {
+        let li = document.createElement('li');
+        li.setAttribute('id', 'keyboard');
+        ulKeyboard.appendChild(li);
+        li.textContent = element.keyLetter;
+    });
+
+    return keyboard1;
+};
+
+let finalKeyboard = displayKeyboard(keyboardMapping);
+
+
+// SELECTING LETTER IS MATCHING WORD'S LETTER ?
+let deleteWord = () => {
+    while(ulWord.firstChild){
+        ulWord.removeChild(ulWord.firstChild);
+    }
+};
+
+// let deleteKeyboard = () => {
+//     while(ulKeyboard.firstChild){
+//         ulKeyboard.removeChild(ulKeyboard.firstChild);
+//     }
+// };
+
+let matchingLetter = (letter) => {
+    // console.log('matchingLetter function is ok');
+    // console.log(letter);
+
+    let matched = false;
+    mapLetter.forEach(element => {
+        if(element.letter === letter){
+            console.log("it's a match");
+            // console.log(element);
+            element.visible = true;
+            matched = true;
+        }
+    });
+
+    if(matched){
+        deleteWord();
+        displayrandomWord(mapLetter);
+    }else{
+        // console.log('nul');
+        count++;
+        // console.log(count);
+        p.textContent = `${count}/10`;
+
+        if(count >= 10){
+            // console.log('GAME OVER');
+            p.textContent = 'GAME OVER';
+        }
+    }
+};
+
+keyboard.addEventListener('click', ({target}) => {
+    // let selectedLetter = target.matches('li');
+    if(target.matches('li')){
+        // console.log(`touché: ${target.innerHTML}`);
+        let selectedLetter = target.innerHTML;
+        matchingLetter(selectedLetter);
+    };
+});
+
+// matchingLetter(mapLetter, finalKeyboard);
+
+
+// CLICK => CHANGE WORD
+btn.addEventListener('click', (actual_word) => {
+    // actual_word = words[Math.floor(Math.random() * words.length)];
+    // console.log(`actual word: ${actual_word}`);
+    // return actual_word;
+
+    document.location.reload();
+
+    // let mapLetter1 = mappingLetter(actual_word);
+    // let wordIsDisplayed1 = 
+    // displayrandomWord(mapLetter1);
+});
 
 // créer clavier virtuel
 //  - liste de chaque lettre
 //  - display liste
 //  - permettre le click + action
-
